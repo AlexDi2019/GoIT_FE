75d80697bd0440a2d7c93718e146315d10eee31f
Александр Дяченко <AlexDi_Omen@outlook.com>
2020-09-25 22:06:27 +0300

    add script

--- JS-Module/script.js
+++ JS-Module/script.js
@@ -1,13 +1,11 @@
-let name = 'название «Генератор защитного поля»';
-const price = 1000;
-let firstMessage = `Выбран «Генератор защитного поля», цена за штуку 1000 кредитов
-`;
-const price = 2000;
-let secondMessage = `Выбран «Генератор защитного поля», цена за штуку 2000 кредитов
-`;
+const name = "«Генератор защитного поля»"; // Write code on this line
+let price = 1000; // Write code on this line
+const firstMessage = `Выбран «Генератор защитного поля», цена за штуку 1000 кредитов`; // Write code on this line
+price = 2000;
+const secondMessage = `Выбран «Генератор защитного поля», цена за штуку 2000 кредитов`; // Write code on this line
 
-console.log(firstMessage);
+// console.log(firstMessage);
 // 'Выбран «Генератор защитного поля», цена за штуку 1000 кредитов'
 
-console.log(secondMessage);
+// console.log(secondMessage);
 // 'Выбран «Генератор защитного поля», цена за штуку 2000 кредитов'
